9197357234b941a5d87b3b225b5cdc445323edfb
Barbora Lendacka <borka34@gmail.com>
2025-10-31 00:53:44 +0100

    Add initial input values to registration form

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,6 +1,20 @@
 import './Registration.style.css'
+import { useState } from 'react'
+
+interface User {
+    username: string,
+    email: string,
+    password: string,
+    passwordConfirm: string
+}
 
 export const Registration = () => {
+    const [user,setUser] = useState<User>({
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+    })
 
     return (
         <>
@@ -12,24 +26,27 @@ export const Registration = () => {
                     <label htmlFor="email">Email Address</label>
                     <input 
                         type="email" id="email" placeholder="Enter your email"
-
+                        value={user.email}
                         
                     />
                     <label htmlFor="username">User Name</label>
                     <input 
                         type="text" id="username" placeholder="Choose a username"
-
+                        value={user.username}
 
                     />
                     <label htmlFor="password">Password</label>
                     <input 
                         type="password" id="password" placeholder="Enter password"
+                        value={user.password}
                         
                     />
 
                     <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="password" id="confirm-password" placeholder="Confirm password"
-                    
+                    <input 
+                        type="password" id="confirm-password" placeholder="Confirm password"
+                        value={user.passwordConfirm}
+
                     />
 
                     <button type="submit">Register</button>
